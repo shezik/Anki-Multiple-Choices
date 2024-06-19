98eef6400a32c3a4dc11f83aca2f7136e562bb2d
Kerry Shen <47771243+shezik@users.noreply.github.com>
2024-06-19 10:58:39 +0800

    Upload files

--- src/template2/front.js
+++
@@ -1,9 +0,0 @@
-function selectButton() {
-    Persistence.setItem("choosenButton", this.getAttribute("Index"));
-    for(let currentlySelected of document.getElementsByClassName("multiple-choice-selected")){
-        currentlySelected.classList.remove("multiple-choice-selected");
-    }
-    this.classList.add("multiple-choice-selected");
-}
-
-init(selectButton);
